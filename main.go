a128c6a357cf4cb1e58cff0049cf0912777889ae
vinit <vinitk81144@gmail.com>
2026-01-18 21:20:07 +0530

    added topological sort

--- main.go
+++ main.go
@@ -71,6 +71,40 @@ func HasCycle(dag *DAG) bool {
 	return visited != len(dag.Nodes)
 }
 
+func TopologicalSort(dag *DAG) ([]string, error) {
+	queue := []string{}
+	order := []string{}
+
+	inDeg := make(map[string]int)
+	maps.Copy(inDeg, dag.InDegree)
+
+	for nodeId, deg := range inDeg {
+		if deg == 0 {
+			queue = append(queue, nodeId)
+		}
+	}
+
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+
+		order = append(order, current)
+
+		for _, neighbour := range dag.Edges[current] {
+			inDeg[neighbour]--
+			if inDeg[neighbour] == 0 {
+				queue = append(queue, neighbour)
+			}
+		}
+	}
+
+	if len(order) != len(dag.Nodes) {
+		return nil, fmt.Errorf("cycle detected, can't topological sort")
+	}
+
+	return order, nil
+}
+
 func main() {
 	nodes := []Node{
 		{ID: "1", Type: "triggerManually"},
@@ -85,6 +119,13 @@ func main() {
 
 	dag := BuildDAG(nodes, edges)
 
+	order, err := TopologicalSort(&dag)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
 	fmt.Println("Has cycle?", HasCycle(&dag))
 	fmt.Println("InDegrees:", dag.InDegree)
+	fmt.Println("Execution order:", order)
 }
